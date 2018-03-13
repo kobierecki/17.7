3d17636e7d9f26ffa557dd7ce4d3b9ebd2a90b19
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2018-03-13 16:12:53 +0100

    17.7

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '350554464077-a761kiq4mks3gi34tvnu7u0scr6hqs2a.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'OVhqc94Ksi_YruNm_3U-gcln',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+
